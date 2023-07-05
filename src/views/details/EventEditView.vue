ac8d22a13183c786f7adc0f520ea118767e2eb89
ph1r1 <phiriyakorn_m@cmu.ac.th>
2023-07-05 16:52:20 +0700

    (Lab04) 5.2* flash message

--- src/views/details/EventEditView.vue
+++ src/views/details/EventEditView.vue
@@ -1,19 +1,38 @@
 <script setup lang="ts">
 import { type CardItem } from '@/type'
 import type { PropType } from 'vue'
+import { useRouter } from 'vue-router'
+import { useMessageStore } from '@/stores/message'
+import { storeToRefs } from 'pinia'
 
-defineProps({
+const props = defineProps({
   event: {
     type: Object as PropType<CardItem>,
     require: true
   }
 })
+
+const router = useRouter()
+const store = useMessageStore()
+const { message } = storeToRefs(store)
+
+const edit = () => {
+  store.updateMessage('The update is in progress...')
+  setTimeout(() => {
+    store.resetMessage()
+    router.push({ name: 'home' })
+  }, 5000)
+}
 </script>
 
 <template>
   <div class="center">
-      <p>Edit the <span>detail</span> here &nbsp;</p>
-      <button>Edit Me</button>
+    <div class="edit-body">
+      <div id="flashMessage" v-if="message">
+        <h4>{{ message }}</h4>
+      </div>
+      <p>Edit the <span>detail</span> here &nbsp;<button @click="edit">Edit Me</button></p>
+    </div>
   </div>
 </template>
 
@@ -26,5 +45,14 @@ defineProps({
 }
 span {
   font-weight: bold;
+}
+#flashMessage {
+  border: 1px solid rgba(74,194,107,0.4);
+  padding: 20px 16px;
+  border-radius: 6px;
+  background-color: #dafbe1;
+  color: #1f2328;
+  margin-bottom: 20px;
+  font-size: 14px;
 }
 </style>
